8dc8538f47774cff84dfd6e148f5ec2a0ed8094a
Jan-Willem Baart <jwbaart@gmail.com>
2020-11-20 13:34:32 +0100

    build: types

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-const cheerio = require("cheerio");
-const puppeteer = require("puppeteer");
+import cheerio from "cheerio";
+import puppeteer from "puppeteer";
 
 const url =
   "https://www.nike.com/nl/w/heren-sale-nike-air-max-lifestyle-13jrmz1m67gz3yaepz7yfbz98pddznik1";
 
+const productLinkSelector = ".product-card__link-overlay";
+const productTitleSelector = ".product-card__title";
+const searchTitle = "Nike Air Max 90";
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(url);
 
   const html = await page.content();
-
   const $ = cheerio.load(html);
-
   const $products = $(".product-card");
-  const productLinkSelector = ".product-card__link-overlay";
-  const productTitleSelector = ".product-card__title";
-  const searchTitle = "Nike Air Max 90";
-
   const getProductLink = (i: number, el: any) =>
     $(el).find(productLinkSelector).attr("href");
 
