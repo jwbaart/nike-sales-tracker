5d992509ac48886990d23c8e7b42b725276210b5
Jan-Willem Baart <jwbaart@gmail.com>
2020-11-20 14:00:55 +0100

    refactor: moved logic to cloud function

---
+++ functions/src/extract-products.ts
@@ -0,0 +1,33 @@
+import cheerio from "cheerio";
+import puppeteer from "puppeteer";
+
+export const extractProductLinks = async (
+  url: string,
+  productLinkSelector: string,
+  productTitleSelector: string,
+  searchTitle: string
+) => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  const $products = $(".product-card");
+  const getProductLink = (i: number, el: any) =>
+    $(el).find(productLinkSelector).attr("href");
+
+  const productContainsSearchTitle = (i: number, el: any) =>
+    $(el).find(productTitleSelector).text().includes(searchTitle);
+
+  const productLinks = $products
+    .filter(productContainsSearchTitle)
+    .map(getProductLink)
+    .get();
+
+  browser.disconnect();
+  return productLinks;
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,35 @@
 import * as functions from "firebase-functions";
+import { extractProductLinks } from "./extract-products";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+export const helloWorld = functions
+  .runWith({
+    timeoutSeconds: 120,
+    memory: "2GB",
+  })
+  .https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+  });
+
+export const productLinks = functions.https.onRequest(
+  async (request, response) => {
+    const url =
+      "https://www.nike.com/nl/w/heren-sale-nike-air-max-lifestyle-13jrmz1m67gz3yaepz7yfbz98pddznik1";
+
+    const productLinkSelector = ".product-card__link-overlay";
+    const productTitleSelector = ".product-card__title";
+    const searchTitle = "Nike Air Max 90";
+
+    const productLinksResult = await extractProductLinks(
+      url,
+      productLinkSelector,
+      productTitleSelector,
+      searchTitle
+    );
+
+    response.send(productLinksResult);
+  }
+);
